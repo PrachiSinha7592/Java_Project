89ff5a673fc120feb2d2018476635ade9d22388b
PrachiSinha7592 <sinhaprachi7592@gmail.com>
2026-02-16 11:19:33 +0530

    Updated File

--- Demo_Jenkins.java
+++ Demo_Jenkins.java
@@ -1,5 +1,5 @@
 public class Demo_Jenkins{
-  public static void main(Strings[] args){
+  public static void main(String[] args){
     int a = 10;
     int b = 20;
     System.out.println("Multiplication = " + (a*b));
